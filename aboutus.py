4c3cc2126219971985557b042422ae3d5338c99e
Aarushi Jain <144267641+aarushijain-24@users.noreply.github.com>
2024-08-10 22:53:40 +0530

    Create aboutus.py

---
+++ aboutus.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def app():
+    st.subheader('Welcome to the Smart Bin project!')
+    st.subheader('Our goal is to revolutionize waste management using smart technology.')
+    st.markdown('Our Mission')
+    st.markdown('To provide a cleaner and more efficient waste management solution that helps in reducing pollution and promoting recycling.')
+    
