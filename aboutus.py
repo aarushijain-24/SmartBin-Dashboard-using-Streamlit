dc69e4e639ac58a8d9a38389226146171a41b375
Aarushi Jain <144267641+aarushijain-24@users.noreply.github.com>
2024-08-14 21:04:01 +0530

    Update aboutus.py

--- aboutus.py
+++ aboutus.py
@@ -3,6 +3,9 @@ import streamlit as st
 def app():
     st.subheader('Welcome to the Smart Bin project!')
     st.subheader('Our goal is to revolutionize waste management using smart technology.')
+    st.markdown('Our core solution, the Smart Bin, integrates advanced IoT technology, deep learning models, and real-time data analytics to automate and enhance waste segregation, monitoring, and collection. The system’s four-compartment waste segregation ensures precise sorting, while our comprehensive information-sharing platform offers valuable insights and analytics, empowering waste management teams to make data-driven decisions with ease.')
     st.markdown('Our Mission')
     st.markdown('To provide a cleaner and more efficient waste management solution that helps in reducing pollution and promoting recycling.')
+    st.markdown('Our Vision')
+    st.markdown('A future where technology plays a pivotal role in minimizing waste, maximizing recycling, and creating cleaner cities for a greener, more sustainable world.')
     
